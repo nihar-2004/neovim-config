2b9dc92b8d04deb37e17cf78483c1ceee64eb64e
nihar-2004 <nihar9363@gmail.com>
2024-11-15 19:51:32 +0530

    added more plugins

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,8 +1,10 @@
 return {
-	'akinsho/bufferline.nvim', 
-	version = "*",
-	config = function()
-		vim.opt.termguicolors = true
-		require("bufferline").setup{}
-	end
+    "akinsho/bufferline.nvim",
+    version = "*",
+    config = function()
+        vim.opt.termguicolors = true
+        require("bufferline").setup({
+--            options = { separator_style = "slant" }
+        })
+    end,
 }
