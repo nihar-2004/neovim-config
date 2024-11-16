9a43691d7db409bb043daf5377a8e25fbcfaa694
nihar-2004 <nihar9363@gmail.com>
2024-11-16 18:58:00 +0530

    some modifications

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,8 +5,9 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
+vim.cmd("set relativenumber")
 
-vim.o.foldcolumn = '5' -- '0' is not bad
+vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
