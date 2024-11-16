1f574b201af44fceacd335eb8945e4e162dfb03e
nihar-2004 <nihar9363@gmail.com>
2024-11-16 19:18:43 +0530

    removed fold level numbers

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,11 +8,10 @@ vim.cmd("set number")
 vim.cmd("set relativenumber")
 
 vim.o.foldcolumn = '1' -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevel = 99 -- using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-vim.o.foldcolumn = '1'
 
 vim.keymap.set("n", "<leader>rn", ":set relativenumber!<CR>")
 vim.keymap.set("n", "<leader>an", ":set number!<CR>")
