719e87698f70ecf87e7683334e526967fdf9a25b
nihar-2004 <nihar9363@gmail.com>
2024-11-16 09:52:07 +0530

    added autocomplete

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,31 +1,34 @@
 return {
 	{
-    	"williamboman/mason.nvim",
-	config = function()
-		require("mason").setup()
-	end
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls"  }
+				ensure_installed = { "lua_ls" },
 			})
-		end
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({})
-			lspconfig.pylsp.setup({})
-			lspconfig.clangd.setup({})
-			lspconfig.rust_analyzer.setup({})
-			vim.keymap.set('n','K',vim.lsp.buf.hover,{})
-			vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', {})
-			vim.keymap.set({'n','v'}, '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', {})
-		end
-	}
-
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pylsp.setup({ capabilities = capabilities })
+			lspconfig.clangd.setup({ capabilities = capabilities })
+			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities })
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", {})
+		end,
+	},
 }
